649d9226a0e431e996395ee4368ad89b2597458f
gseptarisa-ctrl <gseptarisa@gmail.com>
2026-05-19 16:21:36 +0700

    menambahkan class pelajar

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -18,3 +18,11 @@ public:
         cout << "pekerja dibuat\n " << umur << endl;
     }
 };
+
+class pelajar : virtual public orang {
+public:
+    pelajar(int pUmur) : 
+    orang(pUmur) {
+        cout << "pelajar dibuat\n " << umur << endl;
+    }
+};
