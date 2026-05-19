1a54c0d6b854051608cb5e879fba171eb31b4ed5
gseptarisa-ctrl <gseptarisa@gmail.com>
2026-05-19 16:22:23 +0700

    menambahkan class budi dengan akses public ke pekerja dan pelajar

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -26,3 +26,12 @@ public:
         cout << "pelajar dibuat\n " << umur << endl;
     }
 };
+
+class budi : public pekerja, public pelajar {
+public:
+
+    budi(int pUmur) : 
+    orang(pUmur), pekerja(pUmur), pelajar(pUmur) {
+        cout << "budi dibuat\n " << umur << endl;
+    }
+};
